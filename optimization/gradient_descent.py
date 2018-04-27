163e618e9ad0d7c679edc16965480d4d965d1f9e
Petey <pdotjpr@gmail.com>
2018-04-26 18:45:15 -0700

    support for generalized models

--- optimization/gradient_descent.py
+++ optimization/gradient_descent.py
@@ -6,15 +6,16 @@ from autograd import elementwise_grad as egrad
 def squared_error_cost(a,b):
     return np.sum(np.power(a - b,2))
 
-def gradient_descent(train_ex, solutions, cost_fn=squared_error_cost, alpha=.001, grad_thresh=.001):
-    global_cost = lambda theta: cost_fn(np.dot(train_ex,theta), solutions)
+def linear_model(dat, theta):
+    return np.dot(dat,theta)
+
+def gradient_descent(train_ex, solutions, model=linear_model, cost_fn=squared_error_cost, alpha=.001, epochs=10000):
+    global_cost = lambda theta: cost_fn(model(train_ex,theta), solutions)
     grad_fn = egrad(global_cost)
     curr_theta = np.zeros((train_ex.shape[1],1))
-    while True:
+    for i in range(epochs):
         curr_grad = grad_fn(curr_theta)
         curr_theta -= alpha*curr_grad
-        if(np.linalg.norm(curr_grad) < grad_thresh):
-            break;
     return curr_theta
 
 
