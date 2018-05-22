a3a4eec8697f1d31e60159ee9c045b6556cbb98d
Petey <pdotjpr@gmail.com>
2018-05-22 15:23:46 -0700

    added basic SGD

--- optimization/gradient_descent.py
+++ optimization/gradient_descent.py
@@ -22,7 +22,7 @@ def gradient_descent(train_ex, solutions, grad_fn, alpha=.0001, iterations=10000
     for i in range(iterations):
         curr_grad = grad_fn(train_ex,solutions,curr_theta)
         curr_theta -= alpha*np.ndarray.flatten(curr_grad)
-    if(normalize):
+    if normalize:
         return (curr_theta,train_norm_params,sol_norm_params)
     return curr_theta
 
@@ -31,5 +31,12 @@ def adam_optimize(train_ex, solutions, grad_fn, mini_batch_size, alpha = .0001, 
         (train_ex, train_norm_params) = column_normalize(train_ex)
         (solutions,sol_norm_params) = column_normalize(solutions)
     curr_theta = np.zeros((train_ex.shape[1],))
+    for i in range(iterations):
+        indices = np.random.choice(train_ex.shape[0],)
+        min_batch = np.take(train_ex,indices,axis=0)
+        sol_batch = np.take(solutions, indices, axis=0)
+        curr_grad = grad_fn(min_batch, sol_batch, curr_theta)
+        curr_theta -= np.ndarray.flatten(curr_grad)
+    if normalize: 
+        return (curr_theta,train_norm_params,sol_norm_params)
     return curr_theta
-    #To be continued!!! 
