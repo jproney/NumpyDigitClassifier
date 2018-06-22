230ed7248000150251e133cbfd809c7221e6b0ea
Petey <pdotjpr@gmail.com>
2018-06-21 19:16:19 -0700

    added error tracking

--- optimization/gradient_descent.py
+++ optimization/gradient_descent.py
@@ -14,19 +14,27 @@ def column_normalize(array):
     std_vals = array.std(axis=0) + .00001
     return (np.subtract(array,mean_vals)/std_vals, (mean_vals,std_vals))
 
-def gradient_descent(train_ex, solutions, grad_fn, alpha=.0001, iterations=1000, normalize = True):
+def gradient_descent(train_ex, solutions, grad_fn, alpha=.0001, iterations=1000, normalize = True, track_err = False):
+    if track_err:
+        err = np.zeros(iterations)
     if normalize:
         (train_ex, train_norm_params) = column_normalize(train_ex)
         (solutions,sol_norm_params) = column_normalize(solutions)
     curr_theta = np.zeros((train_ex.shape[1],))
     for i in range(iterations):
         curr_grad = grad_fn(train_ex,solutions,curr_theta)
+        if track_err:
+            err[i] = squared_error_cost(train_ex @ curr_theta,solutions)
         curr_theta -= alpha*np.ndarray.flatten(curr_grad)
     if normalize:
+        if track_err:
+            return (curr_theta,train_norm_params,sol_norm_params,err) 
         return (curr_theta,train_norm_params,sol_norm_params)
     return curr_theta
 
-def sgd_optimize(train_ex, solutions, grad_fn, mini_batch_size, alpha = .0001, iterations = 1000, normalize = True):
+def sgd_optimize(train_ex, solutions, grad_fn, mini_batch_size, alpha = .0001, iterations = 1000, normalize = True, track_err = False):
+    if track_err:
+        err = np.zeros(iterations)
     if normalize: 
         (train_ex, train_norm_params) = column_normalize(train_ex)
         (solutions,sol_norm_params) = column_normalize(solutions)
@@ -36,7 +44,11 @@ def sgd_optimize(train_ex, solutions, grad_fn, mini_batch_size, alpha = .0001, i
         min_batch = np.take(train_ex,indices,axis=0)
         sol_batch = np.take(solutions, indices, axis=0)
         curr_grad = grad_fn(min_batch, sol_batch, curr_theta)
+        if track_err:
+            err[i] = squared_error_cost(min_batch @ curr_theta,sol_batch)
         curr_theta -= alpha*np.ndarray.flatten(curr_grad)
     if normalize: 
+        if track_err:
+            return (curr_theta,train_norm_params,sol_norm_params,err) 
         return (curr_theta,train_norm_params,sol_norm_params)
     return curr_theta
--- optimization/test_optimization.py
+++ optimization/test_optimization.py
@@ -17,14 +17,17 @@ class LinearTestCase(unittest.TestCase):
         train_ex = np.hstack((input_vec**2,input_vec,np.ones((10000,1))))
         sol = np.ndarray.flatten(train_ex @ b) + np.ndarray.flatten(noise)
         start = time.time()
-        (theta,(in_add,in_scale),(out_add,out_scale)) = gd.gradient_descent(train_ex,sol, gd.linear_squared_error_gradient)
+        (theta,(in_add,in_scale),(out_add,out_scale),err) = gd.gradient_descent(train_ex,sol, gd.linear_squared_error_gradient,iterations = 200,track_err = True)
         print("Converged in {} Seconds".format(time.time()-start))
         train_ex.sort(0)
         te_norm = np.subtract(train_ex, in_add)/in_scale
         h = np.add(np.multiply(te_norm @ theta,out_scale),out_add)
+        plt.subplot(2,1,1)
         plt.plot(input_vec,sol,'bo',linestyle='none')
         input_vec.sort(0)
         plt.plot(input_vec,h,color='r',linewidth=4)
+        plt.subplot(2,1,2)
+        plt.plot(np.arange(err.shape[0]),err)
         plt.show()
 
 
@@ -35,14 +38,17 @@ class LinearTestCase(unittest.TestCase):
         train_ex = np.hstack((input_vec**2,input_vec,np.ones((10000,1))))
         sol = np.ndarray.flatten(train_ex @ b) + np.ndarray.flatten(noise)
         start = time.time()
-        (theta,(in_add,in_scale),(out_add,out_scale)) = gd.sgd_optimize(train_ex,sol, gd.linear_squared_error_gradient, 1000)
+        (theta,(in_add,in_scale),(out_add,out_scale),err) = gd.sgd_optimize(train_ex,sol, gd.linear_squared_error_gradient, 1000, track_err = True)
         print("Converged in {} Seconds".format(time.time()-start))
         train_ex.sort(0)
         te_norm = np.subtract(train_ex, in_add)/in_scale
-        h = np.add(np.multiply(te_norm @ theta,out_scale),out_add)
+        h = np.add(np.multiply(te_norm @ theta,out_scale),out_add) 
+        plt.subplot(2,1,1)
         plt.plot(input_vec,sol,'bo',linestyle='none')
         input_vec.sort(0)
         plt.plot(input_vec,h,color='r',linewidth=4)
+        plt.subplot(2,1,2)
+        plt.plot(np.arange(err.shape[0]),err)
         plt.show()
         
 if __name__ == "__main__":
