33c12ff932ad6ada0f33619e407b48e14ad6b558
Petey <pdotjpr@gmail.com>
2018-06-21 20:16:25 -0700

    momentum

--- optimization/gradient_descent.py
+++ optimization/gradient_descent.py
@@ -52,3 +52,27 @@ def sgd_optimize(train_ex, solutions, grad_fn, mini_batch_size, alpha = .0001, i
             return (curr_theta,train_norm_params,sol_norm_params,err) 
         return (curr_theta,train_norm_params,sol_norm_params)
     return curr_theta
+
+
+def adam_optimize(train_ex, solutions, grad_fn, mini_batch_size, alpha = .0001, gamma = .99, eta = .00005, iterations = 1000, normalize = True, track_err = False):
+    if track_err:
+        err = np.zeros(iterations)
+    if normalize: 
+        (train_ex, train_norm_params) = column_normalize(train_ex)
+        (solutions,sol_norm_params) = column_normalize(solutions)
+    curr_theta = np.zeros((train_ex.shape[1],))
+    momentum = np.zeros((train_ex.shape[1],))
+    for i in range(iterations):
+        indices = np.random.choice(train_ex.shape[0],mini_batch_size)
+        min_batch = np.take(train_ex,indices,axis=0)
+        sol_batch = np.take(solutions, indices, axis=0)
+        curr_grad = grad_fn(min_batch, sol_batch, curr_theta)
+        if track_err:
+            err[i] = squared_error_cost(min_batch @ curr_theta,sol_batch)
+        momentum = gamma*momentum + eta*np.ndarray.flatten(curr_grad)
+        curr_theta -= (alpha*np.ndarray.flatten(curr_grad) + momentum)
+    if normalize: 
+        if track_err:
+            return (curr_theta,train_norm_params,sol_norm_params,err) 
+        return (curr_theta,train_norm_params,sol_norm_params)
+    return curr_theta
--- optimization/test_optimization.py
+++ optimization/test_optimization.py
@@ -11,6 +11,7 @@ def fuzzy_equals(a,b):
 class LinearTestCase(unittest.TestCase):
     
     def testBasicGD(self):
+        print("testing gradient descent...")
         b = np.random.rand(3,1)*20
         noise = np.random.rand(10000,1)*20000
         input_vec = np.random.rand(10000,1)*100
@@ -32,6 +33,7 @@ class LinearTestCase(unittest.TestCase):
 
 
     def testSGD(self):
+        print("testing SGD...")
         b = np.random.rand(3,1)*20
         noise = np.random.rand(10000,1)*20000
         input_vec = np.random.rand(10000,1)*100
@@ -39,6 +41,28 @@ class LinearTestCase(unittest.TestCase):
         sol = np.ndarray.flatten(train_ex @ b) + np.ndarray.flatten(noise)
         start = time.time()
         (theta,(in_add,in_scale),(out_add,out_scale),err) = gd.sgd_optimize(train_ex,sol, gd.linear_squared_error_gradient, 1000, track_err = True)
+        print("Converged in {} Seconds".format(time.time()-start))
+        train_ex.sort(0)
+        te_norm = np.subtract(train_ex, in_add)/in_scale
+        h = np.add(np.multiply(te_norm @ theta,out_scale),out_add) 
+        plt.subplot(2,1,1)
+        plt.plot(input_vec,sol,'bo',linestyle='none')
+        input_vec.sort(0)
+        plt.plot(input_vec,h,color='r',linewidth=4)
+        plt.subplot(2,1,2)
+        plt.plot(np.arange(err.shape[0]),err)
+        plt.show()
+
+
+    def testAdam(self):
+        print("testing ADAM...")
+        b = np.random.rand(3,1)*20
+        noise = np.random.rand(10000,1)*20000
+        input_vec = np.random.rand(10000,1)*100
+        train_ex = np.hstack((input_vec**2,input_vec,np.ones((10000,1))))
+        sol = np.ndarray.flatten(train_ex @ b) + np.ndarray.flatten(noise)
+        start = time.time()
+        (theta,(in_add,in_scale),(out_add,out_scale),err) = gd.adam_optimize(train_ex,sol, gd.linear_squared_error_gradient, 1000, track_err = True)
         print("Converged in {} Seconds".format(time.time()-start))
         train_ex.sort(0)
         te_norm = np.subtract(train_ex, in_add)/in_scale
