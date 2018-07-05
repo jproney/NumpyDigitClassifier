d94a485a8cd5e2db8bd569b7697665034430ef4d
Petey <pdotjpr@gmail.com>
2018-07-04 17:30:32 -0700

    75 percent accuracy on MNIST, that's not too bad!

--- optimization/gradient_descent.py
+++ optimization/gradient_descent.py
@@ -6,7 +6,7 @@ def squared_error_cost(train_ex,sol,theta):
     h = train_ex @ theta
     return np.sum(np.power(h - sol,2))/train_ex.shape[0]
 
-def linear_squared_error_gradient(train_ex, sol, theta):
+def squared_error_cost_gradient(train_ex, sol, theta):
     h = train_ex @ theta 
     return train_ex.T @ (h - sol)/train_ex.shape[0]
 
@@ -16,17 +16,31 @@ def sigmoid(train_ex, theta):
 
 def logistic_cost(train_ex,sol,theta):
     h = sigmoid(train_ex,theta)
-    return -np.sum(sol.T @ np.log(h) + (1-sol).T @ np.log(1-h))/train_ex.shape[0]    
+    return -(sol.T @ np.log(h) + (1-sol).T @ np.log(1-h))/train_ex.shape[0]    
 
 def logistic_cost_gradient(train_ex,sol,theta):
     return train_ex.T @ (sigmoid(train_ex,theta)-sol)/train_ex.shape[0]
 
-def percent_wrong_binary_class(train_ex,sol,theta):
-    return np.sum(np.square(np.round(sigmoid(train_ex,theta)) - sol))/train_ex.shape[0]
+def softmax(train_ex,theta):
+    e = np.exp(train_ex @ theta)
+    return e/(np.sum(e))
+
+def cross_entropy_cost(train_ex,sol,theta):
+    h = softmax(train_ex,theta)
+    return -np.sum(sol.T @ np.log(h) + (1-sol).T @ np.log(1-h))/train_ex.shape[0]/theta.shape[1]
+
+def cross_entropy_cost_gradient(train_ex, sol, theta):
+    return train_ex.T @ (softmax(train_ex,theta) - sol)/train_ex.shape[0]/theta.shape[1]
+
+def classification_accuracy(train_ex,sol,theta):
+    guess = np.zeros(sol.shape)
+    guess_idx = np.argmax(softmax(train_ex,theta), axis=1)
+    guess[range(guess.shape[0]),guess_idx] = 1
+    return np.sum(np.square(guess - sol))/train_ex.shape[0]/2
 
 def column_normalize(array):
     mean_vals = array.mean(axis=0)
-    std_vals = array.std(axis=0) + .00001
+    std_vals = array.std(axis=0) + .000001
     return (np.subtract(array,mean_vals)/std_vals, (mean_vals,std_vals))
 
 def gradient_descent(train_ex, solutions, grad_fn, alpha=.5, iterations=1000, track_err = False, error_fn = None,init_theta = None,track_progress = False, num_logs= 100):
@@ -81,8 +95,8 @@ def adam_optimize(train_ex, solutions, grad_fn, mini_batch_size, alpha = .5, gam
     if init_theta is None:
         init_theta = np.zeros(train_ex.shape[1],)
     curr_theta = init_theta
-    y = np.zeros((train_ex.shape[1],))
-    y_last = np.zeros((train_ex.shape[1],))
+    y = np.zeros(init_theta.shape)
+    y_last = np.zeros(init_theta.shape)
     for i in range(iterations):
         if track_progress and i%(iterations/100) == 0:
             print("{}% complete".format((i/iterations)*100))
--- optimization/test_optimization.py
+++ optimization/test_optimization.py
@@ -18,7 +18,7 @@ class LinearTestCase(unittest.TestCase):
         start = time.time()
         (train_ex, (in_add,in_scale)) = gd.column_normalize(train_ex)
         (sol,(out_add,out_scale)) = gd.column_normalize(sol)
-        (theta,err) = gd.gradient_descent(train_ex,sol, gd.linear_squared_error_gradient,iterations = 200,track_err = True, error_fn = gd.squared_error_cost)
+        (theta,err) = gd.gradient_descent(train_ex,sol, gd.squared_error_cost_gradient,iterations = 200,track_err = True, error_fn = gd.squared_error_cost)
         print("Converged in {} Seconds".format(time.time()-start))
         train_ex.sort(0)
         h = train_ex @ theta
@@ -41,7 +41,7 @@ class LinearTestCase(unittest.TestCase):
         start = time.time()
         (train_ex, (in_add,in_scale)) = gd.column_normalize(train_ex)
         (sol,(out_add,out_scale)) = gd.column_normalize(sol)
-        (theta,err) = gd.sgd_optimize(train_ex,sol, gd.linear_squared_error_gradient,1000,iterations = 1000,track_err = True,error_fn = gd.squared_error_cost)
+        (theta,err) = gd.sgd_optimize(train_ex,sol, gd.squared_error_cost_gradient,1000,iterations = 1000,track_err = True,error_fn = gd.squared_error_cost)
         print("Converged in {} Seconds".format(time.time()-start))
         train_ex.sort(0)
         h = train_ex @ theta
@@ -64,7 +64,7 @@ class LinearTestCase(unittest.TestCase):
         start = time.time()
         (train_ex, (in_add,in_scale)) = gd.column_normalize(train_ex)
         (sol,(out_add,out_scale)) = gd.column_normalize(sol)
-        (theta,err) = gd.adam_optimize(train_ex,sol, gd.linear_squared_error_gradient,1000,iterations = 1000,track_err = True,error_fn = gd.squared_error_cost)
+        (theta,err) = gd.adam_optimize(train_ex,sol, gd.squared_error_cost_gradient,1000,iterations = 1000,track_err = True,error_fn = gd.squared_error_cost)
         print("Converged in {} Seconds".format(time.time()-start))
         train_ex.sort(0)
         h = train_ex @ theta
@@ -81,12 +81,13 @@ class LinearTestCase(unittest.TestCase):
         m = mnist.MNIST('/home/petey/Documents/PythonProjects/MachineLearning/python-mnist/data')
         images, labels = m.load_training()
         train_ex = np.asarray(images)
-        sol = (np.asarray(labels) == 7).astype('uint8')
+        labels = np.tile(np.asarray(labels),(10,1)).T
+        cats = np.tile(np.arange(0,10),(train_ex.shape[0],1))
+        sol = np.equal(labels, cats).astype('uint8')
+        theta0 = np.random.rand(train_ex.shape[1], sol.shape[1])
         (train_ex, (in_add, in_scale)) = gd.column_normalize(train_ex) 
-        (theta,err) = gd.adam_optimize(train_ex,sol,gd.logistic_cost_gradient,32,iterations = 400000,alpha = .005, gamma=0.9, 
-            init_theta = np.random.rand(train_ex.shape[1]) - 1, track_err = True,error_fn = gd.percent_wrong_binary_class,track_progress = True)
-        #(theta,err) = gd.gradient_descent(train_ex,sol,gd.logistic_cost_gradient,iterations = 5000,alpha = .001,
-        #    track_err = True,error_fn = gd.logistic_cost,track_progress = True)
+        (theta,err) = gd.adam_optimize(train_ex,sol,gd.cross_entropy_cost_gradient,32,iterations = 50000,alpha = .005, gamma=0.9, 
+            init_theta = theta0, track_err = True,error_fn = gd.classification_accuracy,track_progress = True)
         plt.plot(np.arange(err.shape[0]),err)
         print(err[-1])
         plt.show()        
