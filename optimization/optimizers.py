ec69745e7ed777010d77cdd276cc2ce107ea6f9c
Petey <pdotjpr@gmail.com>
2019-05-31 16:37:42 -0700

    new gradient descent unit tests

--- optimization/optimizers.py
+++ optimization/optimizers.py
@@ -1,4 +1,5 @@
 # gradient descent-based algorithms for minimizing cost
+import numpy as np
 
 
 def gradient_descend(grad_fn, init_theta, data_stream, alpha):
@@ -6,16 +7,19 @@ def gradient_descend(grad_fn, init_theta, data_stream, alpha):
     Simple gradient descent algorithm for minimizing objective
     """
 
-    curr_theta = init_theta
+    curr_theta = np.copy(init_theta)
 
-    for aux_data in data_stream():
-
+    for aux_data in data_stream:
         curr_grad = grad_fn(curr_theta, aux_data)
         curr_theta -= alpha * curr_grad
         yield curr_theta
 
 
+def dummy_data_stream(steps):
+    for x in range(steps):
+        yield None
 
 
-def mini_batch_generator():
-    pass
+def full_batch_stream(X, y, steps):
+    for x in range(steps):
+        yield X, y
--- optimization/test_optimization.py
+++ optimization/test_optimization.py
@@ -1,30 +1,73 @@
-import time
 import unittest
-
 from optimization import optimizers as opt
 from optimization import loss_functions as lf
-from optimization import utils
-import matplotlib.pyplot as plt
-import mnist
 import numpy as np
 
 
-class LinearTestCase(unittest.TestCase):
+def fuzzy_equals(a, b, eps):
+    return sum(abs(a-b)) < eps
 
-    def test_basic_GD(self):
-        print("testing gradient descent...")
-        (theta, err) = opt.gradient_descent(train_ex, sol, lf.squared_error_cost_gradient, epochs=200, track_err=True,
-                                            error_fn=lf.squared_error_cost)
-        print("Converged in {} Seconds".format(time.time() - start))
-        train_ex.sort(0)
-        h = train_ex @ theta
-        plt.subplot(2, 1, 1)
-        plt.plot(input_vec, sol, 'bo', linestyle='none')
-        input_vec.sort(0)
-        plt.plot(input_vec, h, color='r', linewidth=4)
-        plt.subplot(2, 1, 2)
-        plt.plot(np.arange(err.shape[0]), err)
-        plt.show()
+
+class OptimizationTests(unittest.TestCase):
+
+    def test_gd_parabaloid(self):
+
+        iters = 200
+
+        # Objective function is parabaloid: (3 - x)**2 + (7 - y)**2
+        minim = np.array([3.0, 7.0])  # Global minimum exists at (3,7)
+
+        def grad(theta, aux_data): return np.array([-2*(3-theta[0]), -2*(7-theta[1])])
+
+        start = np.array([-4.0, 2.0])
+
+        theta = None
+        for theta in opt.gradient_descend(grad_fn=grad, data_stream=opt.dummy_data_stream(iters), alpha=.05, init_theta=start):
+            pass
+
+        assert(fuzzy_equals(theta, minim, .01))
+
+    def test_gd_himmelblau(self):
+
+        iters = 200
+
+        # Himmelblau function : (x**2 + y - 11)**2 + (x + y**2 - 7)**2
+        # 4 minima at: (3.0,2.0), (-2.805118, 3.131312), (-3.779310, -3.283186), (3.584428, -1.848126)
+        minim1 = np.array([3.0, 2.0])
+        minim2 = np.array([-2.805118, 3.131312])
+        minim3 = np.array([-3.779310, -3.283186])
+        minim4 = np.array([3.584428, -1.848126])
+
+        def grad(theta, aux_data): return np.array([4*theta[0]*(theta[0]**2 + theta[1] - 11) + 2*(theta[0] + theta[1]**2 - 7),
+                                                    2*(theta[0]**2 + theta[1] - 11) + 4*theta[1]*(theta[0] + theta[1]**2 - 7)])
+
+        start = np.array([2.0, -3.0])
+
+        theta = None
+        for theta in opt.gradient_descend(grad_fn=grad, data_stream=opt.dummy_data_stream(iters), alpha=.005, init_theta=start):
+            pass
+
+        assert(fuzzy_equals(theta, minim1, .01) or fuzzy_equals(theta, minim2, .01) or fuzzy_equals(theta, minim3, .01)
+               or fuzzy_equals(theta, minim4, .01))
+
+    def test_gd_regression(self):
+
+        iters = 10000
+
+        theta_real = np.array([4.0, -5.3, 2.2, 11.8, 6.4, 0.8])
+        X = np.random.rand(500, 6)
+        y = X @ theta_real
+
+        def grad(theta, aux_data): return lf.squared_error_cost_gradient(aux_data[0], aux_data[1], theta)
+
+        start = np.zeros(6)
+
+        theta = None
+        for theta in opt.gradient_descend(grad_fn=grad, data_stream=opt.full_batch_stream(X, y, iters), alpha=.01, init_theta=start):
+            pass
+
+        assert(fuzzy_equals(theta, theta_real, .1))
+
 
 if __name__ == "__main__":
     unittest.main()
